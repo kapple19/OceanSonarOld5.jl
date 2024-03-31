8bf9403be29ffcb5d55747120df0f17d067c758c
Aaron Kaw <aaronjkaw@gmail.com>
2024-04-01 09:12:32 +1100

    Reworking Makie extension Boundary plotting implemented

--- ext/OceanSonarMakieExt/OceanSonarMakieExt.jl
+++ ext/OceanSonarMakieExt/OceanSonarMakieExt.jl
@@ -4,35 +4,15 @@ using OceanSonar
 using Makie
 
 import OceanSonar: visual!, visual
+import Makie: convert_arguments
 
-function get_plot_arguments(plot::Plot)
-    N = 0
-    while true
-        try
-            N += 1
-            plot[N].val
-        catch
-            N -= 1
-            break
-        end
-    end
-    idxs = 1:N
-    return plot, (plot[n].val for n in idxs)...
-end
+include("boundary.jl")
 
 function visual(args...; kw...)
     fig = Figure()
-    Axis(fig[1, 1])
-    visual!(args...; kw...)
-    return fig
+    axis = Axis(fig[1, 1])
+    _, plot = visual!(axis, args...; kw...)
+    return Makie.FigureAxisPlot(fig, axis, plot)
 end
 
-include("boundary.jl")
-include("bivariate.jl")
-include("medium.jl")
-include("environment.jl")
-include("scenario.jl")
-include("ray.jl")
-include("propagation.jl")
-
-end
+end # module OceanSonarMakieExt
--- ext/OceanSonarMakieExt/boundary.jl
+++ ext/OceanSonarMakieExt/boundary.jl
@@ -1,65 +1,27 @@
 import OceanSonar: boundaryplot!, boundaryplot
 
-function ribbon_data(ati::Altimetry, x_lo::Real = 0.0, x_hi::Real = 1e3)
-    x = range(x_lo, x_hi, 301)
-    z_lo, _ = OceanSonar.depth_extrema(ati, x_lo, x_hi)
-    z_hi = ati.(x)
-    return x, z_lo, z_hi
+@info "Defining boundary recipes."
+
+function convert_arguments(plot_type::PointBased, bnd::Boundary, x::AbstractVector{<:Real})
+    return convert_arguments(plot_type, x, x .|> bnd)
 end
 
-function ribbon_data(bty::Bathymetry, x_lo::Real = 0.0, x_hi::Real = 1e3)
-    x = range(x_lo, x_hi, 301)
+function convert_arguments(plot_type::Type{<:Band}, ati::Altimetry, x::AbstractVector{<:Real})
+    x_lo, x_hi = extrema(x)
+    z_lo, _ = OceanSonar.depth_extrema(ati, x_lo, x_hi)
+    z_hi = x .|> ati
+    return convert_arguments(plot_type, x, z_lo, z_hi)
+end
+
+function convert_arguments(plot_type::Type{<:Band}, bty::Boundary, x::AbstractVector{<:Real})
+    x_lo, x_hi = extrema(x)
     _, z_hi = OceanSonar.depth_extrema(bty, x_lo, x_hi)
     z_lo = bty.(x)
-    return x, z_lo, z_hi
+    return convert_arguments(plot_type, x, z_lo, z_hi)
 end
 
-@recipe(BoundaryPlot) do scene
-    Theme()
-end
-
-function boundaryplot_!(
-    plot::Plot,
-    bnd::Boundary,
-    x_lo::Real = 0.0,
-    x_hi::Real = 1e3
-)
-    band!(plot,
-        ribbon_data(bnd, x_lo, x_hi)...,
-        color = OceanSonar.boundary_colour(bnd),
-        axis = (yreversed = true,) # doesn't work in low-level Makie processing
-    )
-    return plot
-end
-
-function boundaryplot_!(
-    plot::Plot,
-    env::Environment,
-    x_lo::Real = 0.0,
-    x_hi::Real = 1e3
-)
-    boundaryplot_!(plot, env.bnd, x_lo, x_hi)
-end
-
-function boundaryplot_!(plot::Plot, scen::Scenario)
-    x_lo = 0.0
-    x_hi = scen.x
-    boundaryplot_!(plot, scen.env, x_lo, x_hi)
-end
-
-boundaryplot_!(::Plot, args...) = error(
-    "No method matching desired signature for", args, ".\n",
-    "Try one of these:\n",
-    methods(boundaryplot_!)
-)
-
-function Makie.plot!(plot::BoundaryPlot)
-    get_plot_arguments(plot) |> splat(boundaryplot_!)
-end
-
-function visual!(bnd::Boundary, args...; kw...)
-    plot = boundaryplot!(bnd, args...; kw...)
-    ax = current_axis()
-    ax.yreversed = true
-    return current_figure()
+function visual!(axis::Makie.AbstractAxis, bnd::Boundary, x::AbstractVector{<:Real})
+    axis.yreversed = true
+    plot = band!(axis, bnd, x)
+    return Makie.AxisPlot(axis, plot)
 end
